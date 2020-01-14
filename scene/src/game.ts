78d2cc8e769f3fa4754f6e9acf9f5d473b92a224
nearnshaw <nearnshaw@decentraland.org>
2020-01-14 14:25:26 -0300

    hover feedback

--- scene/src/game.ts
+++ scene/src/game.ts
@@ -17,19 +17,26 @@ let refreshTimer: number = refreshInterval
 ///// Systems
 
 export class RotatorSystem implements ISystem {
- 
   update(dt: number) {
     for (let door of doors.entities) {
       let state = door.getComponent(DoorState)
       let transform = door.getComponent(Transform)
-      
+
       if (state.closed == false && state.fraction < 1) {
         state.fraction += dt
-        let rot = Quaternion.Slerp(state.closedPos, state.openPos, state.fraction)
+        let rot = Quaternion.Slerp(
+          state.closedPos,
+          state.openPos,
+          state.fraction
+        )
         transform.rotation = rot
       } else if (state.closed == true && state.fraction > 0) {
         state.fraction -= dt
-        let rot = Quaternion.Slerp(state.closedPos, state.openPos, state.fraction)
+        let rot = Quaternion.Slerp(
+          state.closedPos,
+          state.openPos,
+          state.fraction
+        )
         transform.rotation = rot
       }
     }
@@ -40,44 +47,47 @@ export class RotatorSystem implements ISystem {
 engine.addSystem(new RotatorSystem())
 
 export class CheckServer implements ISystem {
-  update(dt:number){
+  update(dt: number) {
     refreshTimer -= dt
-    if (refreshTimer <0){
+    if (refreshTimer < 0) {
       refreshTimer = refreshInterval
       getFromServer()
     }
   }
-
 }
 engine.addSystem(new CheckServer())
-
 
 /////// Add entities and their components
 
 // Define fixed walls
 const wall1 = new Entity()
-wall1.addComponent(new Transform({
-  position: new Vector3(5.75, 1, 3),
-  scale: new Vector3(1.5, 2, 0.05)
-}))
+wall1.addComponent(
+  new Transform({
+    position: new Vector3(5.75, 1, 3),
+    scale: new Vector3(1.5, 2, 0.05)
+  })
+)
 wall1.addComponent(new BoxShape())
 engine.addEntity(wall1)
 
 const wall2 = new Entity()
-wall2.addComponent(new Transform({
-  position: new Vector3(3.25, 1, 3),
-  scale: new Vector3(1.5, 2, 0.05)
-}))
+wall2.addComponent(
+  new Transform({
+    position: new Vector3(3.25, 1, 3),
+    scale: new Vector3(1.5, 2, 0.05)
+  })
+)
 wall2.addComponent(new BoxShape())
 engine.addEntity(wall2)
 
-
 // Add actual door to scene. This entity doesn't rotate, its parent drags it with it.
 const door = new Entity()
-door.addComponent(new Transform({
-  position: new Vector3(0.5, 0, 0),
-  scale: new Vector3(1, 2, 0.05)
-}))
+door.addComponent(
+  new Transform({
+    position: new Vector3(0.5, 0, 0),
+    scale: new Vector3(1, 2, 0.05)
+  })
+)
 door.addComponent(new BoxShape())
 engine.addEntity(door)
 
@@ -92,9 +102,11 @@ door.addComponent(doorMaterial)
 
 // Define wrapper entity to rotate door. This is the entity that actually rotates.
 const doorPivot = new Entity()
-doorPivot.addComponent(new Transform({
-  position: new Vector3(4, 1, 3)
-}))
+doorPivot.addComponent(
+  new Transform({
+    position: new Vector3(4, 1, 3)
+  })
+)
 doorPivot.addComponent(new DoorState())
 engine.addEntity(doorPivot)
 
@@ -103,30 +115,31 @@ door.setParent(doorPivot)
 
 // Set the click behavior for the door
 door.addComponent(
-  new OnClick(e => {
-    let currentState = door.getParent().getComponent(DoorState)
-    currentState.closed = !currentState.closed
-    callAPI(currentState.closed)
-  })
+  new OnPointerDown(
+    e => {
+      let currentState = door.getParent().getComponent(DoorState)
+      currentState.closed = !currentState.closed
+      callAPI(currentState.closed)
+    },
+    { button: ActionButton.POINTER, hoverText: 'Open/Close' }
+  )
 )
-
 
 ///// Connect to the REST API
 
-const apiUrl = "http://127.0.0.1:7753"
+const apiUrl = 'http://127.0.0.1:7753'
 
 const headers = {
-  Accept: "application/json",
-  "Content-Type": "application/json"
-};
+  Accept: 'application/json',
+  'Content-Type': 'application/json'
+}
 
 // function called when activating door
-function callAPI(closed: boolean){
+function callAPI(closed: boolean) {
   let url
-  if (closed){
+  if (closed) {
     url = `${apiUrl}/api/door/close`
-  }
-  else {
+  } else {
     url = `${apiUrl}/api/door/open`
   }
 
@@ -134,33 +147,29 @@ function callAPI(closed: boolean){
     try {
       let response = await fetch(url)
       let json = await response.json()
-      log("sent request to API " + closed)
+      log('sent request to API ' + closed)
     } catch {
-      log("failed to reach URL")
+      log('failed to reach URL')
     }
   })
   getFromServer()
-
 }
 
 // Function called at regular intervals
-function getFromServer(){
- 
+function getFromServer() {
   let url = `${apiUrl}/api/door/state`
-  
+
   executeTask(async () => {
     try {
       let response = await fetch(url)
       let json = await response.json()
-      log("sent request to API")
+      log('sent request to API')
       log(json)
       doorPivot.getComponent(DoorState).closed = json.state
-        
     } catch {
-      log("failed to reach URL")
+      log('failed to reach URL')
     }
-
-   })
+  })
 }
 
 // Start the scene with the door's state matching the server
